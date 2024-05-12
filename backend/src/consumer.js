596072a233908ac0c10cc55819701bed1273f24c
Oscar Oguledo <oscarchiagoziem@gmail.com>
2024-05-12 03:45:42 +0000

    updated consumer

--- backend/src/consumer.js
+++ backend/src/consumer.js
@@ -9,7 +9,17 @@ const consumerRun = async (groupId, topics) => {
     console.log(
       `Topic - ${topic}, Partition - ${partition}, Message - ${message.value.toString()}`
     );
-  };
+    try {
+          //addmessage(JSON.parse(message.value.toString()));
+          console.log(`Consumer caught ${message.value.toString()} successfully.`);
+      } catch (err) {
+          console.error(`Error processing ${topic}: `, err);
+          pause();
+          setTimeout(() => {
+              consumer.resume([{ topic: topic }]);
+          }, 60 * 1000);
+      }
+    };
 
   try {
     await consumer.run({
